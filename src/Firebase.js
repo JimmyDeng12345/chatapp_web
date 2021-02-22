accd1b05990df5eb66326aa013ce8210e38c0ea3
Jimmy Deng <46387771+JimmyDeng12345@users.noreply.github.com>
2021-02-22 14:58:48 -0700

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,13 +5,7 @@ import 'firebase/auth';
 import 'firebase/analytics';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBjZ-QWn0k8TZ7QhW8XnCVK5mvDoZLxGmI",
-    authDomain: "chat-app-c0207.firebaseapp.com",
-    projectId: "chat-app-c0207",
-    storageBucket: "chat-app-c0207.appspot.com",
-    messagingSenderId: "760182975306",
-    appId: "1:760182975306:web:ea14e80bd7324b91bfafb3",
-    measurementId: "G-HG4S8MSW38"
+    //enter config details
   };
   //firebase.analytics();
 firebase.initializeApp(firebaseConfig);
