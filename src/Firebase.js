506cd2a2e1bc854d8f365e0e40a819708735ad9f
Jimmy <jimmydeng12345@gmail.com>
2021-02-23 11:33:14 -0700

    fixed keys

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,8 +5,14 @@ import 'firebase/auth';
 import 'firebase/analytics';
 
 const firebaseConfig = {
-    //enter config details
-  };
+  apiKey: "AIzaSyBjZ-QWn0k8TZ7QhW8XnCVK5mvDoZLxGmI",
+  authDomain: "chat-app-c0207.firebaseapp.com",
+  projectId: "chat-app-c0207",
+  storageBucket: "chat-app-c0207.appspot.com",
+  messagingSenderId: "760182975306",
+  appId: "1:760182975306:web:ea14e80bd7324b91bfafb3",
+  measurementId: "G-HG4S8MSW38"
+};
   //firebase.analytics();
 firebase.initializeApp(firebaseConfig);
 firebase.functions().useEmulator("localhost", 5001);
